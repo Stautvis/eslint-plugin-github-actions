86a7095a103d0d2c8701517a5001ae9ccad5f120
Tautvydas Steponavičius <t.steponavicius@icloud.com>
2025-06-13 01:22:42 +0300

    chore: #5 Add Prettier support (#6)
    * chore: #5 Add Prettier as a development dependency
    
    * chore: #5 Add Prettier configuration and script to package.json

---
+++ configs/prettier.config.cjs
@@ -0,0 +1,13 @@
+/**
+ * @see https://prettier.io/docs/configuration
+ * @type {import("prettier").Config}
+ */
+const config = {
+  tabWidth: 2,
+  semi: false,
+  singleQuote: true,
+  endOfLine: 'lf',
+  printWidth: 100,
+}
+
+module.exports = config
